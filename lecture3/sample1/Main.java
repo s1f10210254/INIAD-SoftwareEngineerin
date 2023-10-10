6ea0f7401dabee0661a807ec045bfedaef5c1554
s1f10210254 <s1f10210248@iniad.org>
2023-10-10 16:06:02 +0900

    a

--- lecture3/sample1/Main.java
+++ lecture3/sample1/Main.java
@@ -1,20 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        // Constractor and instance field
         Student student = new Student("Taro", 3);
         System.out.printf("%s %d\n", student.name, student.grade );
-
         Student student2 = new Student("Jiro");
         System.out.printf("%s %d\n", student2.name, student2.grade);
 
+        
         if(student.belongsToCourse()){
             System.out.println("Belongs to a couse");
         }
+        
+        //Instance method
         if(student.belongsToCourse(23)) {
             System.out.println("Belongs to a course.");
         }
 
+        //Static field
         System.out.printf("%d stduents in total.\n", Student.cnt);
 
+        //Static method
         if(Student.older(student, student2)) {
             System.out.printf("%s is older than %s.\n", student.name, student2.name);
         }
