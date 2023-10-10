525b11b841af980f3851bb0b91f7f83410732f53
s1f10210254 <s1f10210248@iniad.org>
2023-10-10 17:55:04 +0900

    getCurrenTIme

--- Practice3/sample2/Time.java
+++ Practice3/sample2/Time.java
@@ -1,45 +1,47 @@
 public class Time {
-    int hour;
-    int minute;
-    int second;
+    // int hour;
+    // int minute;
+    // int second;
 
-    public Time(int hour, int minute, int second){
-        this.hour = hour;
-        this.minute = minute;
-        this.second = second;
-    }
+    // public Time(int hour, int minute, int second){
+    //     this.hour = hour;
+    //     this.minute = minute;
+    //     this.second = second;
+    // }
 
-    public int getHour(){
-        return hour;
-    }
+    // public int getHour(){
+    //     return hour;
+    // }
 
-    public void setHour(int hour){
-        this.hour = hour;
-    }
+    // public void setHour(int hour){
+    //     this.hour = hour;
+    // }
     
-    public int getMinute() {
-        return minute;
-    }
+    // public int getMinute() {
+    //     return minute;
+    // }
 
-    public void setMinute(int minute){
-        this.minute = minute;
-    }
+    // public void setMinute(int minute){
+    //     this.minute = minute;
+    // }
 
-    public int getSecond() {
-        return second;
-    }
+    // public int getSecond() {
+    //     return second;
+    // }
     
-    public void setSeccond(int second){
-        this.second = second;
-    }
+    // public void setSeccond(int second){
+    //     this.second = second;
+    // }
 
-    public String getCurrentTime(){
-        return String.format("%02d:%02d:%02d", hour, minute, second);
-    }
 
+    public static String getCurrentTime() {
+        java.util.Calendar now = java.util.Calendar.getInstance();
+        return now.get(java.util.Calendar.HOUR_OF_DAY) + ":" + now.get(java.util.Calendar.MINUTE) + ":" + now.get(java.util.Calendar.SECOND);
+    }
     public static void main(String[] args) {
-        Time time = new Time(18, 00, 45);
-        System.out.println(time.getCurrentTime());
+        // Time time = new Time(18, 00, 45);
+        // System.out.println("Time: " + time.getHour() + ":" + time.getMinute() + ":" + time.getSecond());
+        System.out.println("Current Time: " + Time.getCurrentTime());
     }
 
 
