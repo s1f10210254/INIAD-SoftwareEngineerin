ee0d48e894cdb4b169e7afe91cce071c8cb425bc
s1f10210254 <s1f10210248@iniad.org>
2023-10-10 15:57:11 +0900

    static

--- lecture3/sample1/Main.java
+++ lecture3/sample1/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("Taro", 3);
-        System.out.printf("%s %d\n", student.name, student.grade);
+        System.out.printf("%s %d\n", student.name, student.grade );
 
         Student student2 = new Student("Jiro");
         System.out.printf("%s %d\n", student2.name, student2.grade);
@@ -12,5 +12,7 @@ public class Main {
         if(student.belongsToCourse(23)) {
             System.out.println("Belongs to a course.");
         }
+
+        System.out.printf("%d stduents in total.\n", Student.cnt);
     }
 }
--- lecture3/sample1/Student.java
+++ lecture3/sample1/Student.java
@@ -3,10 +3,12 @@ public class Student {
 
     String name;
     int grade;
+    static int cnt = 0;
     // constructor
     Student(String name, int grade){
         this.name = name;
         this.grade = grade;
+        cnt ++;
     }
     Student(String name){
         this(name, 1);
