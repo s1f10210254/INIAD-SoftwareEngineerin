b13d0eb19b1bc15aec3b10a1bf9923ac1a5e7022
s1f10210254 <s1f10210248@iniad.org>
2023-10-10 14:52:41 +0900

    lec3

---
+++ lecture3/Quiz.java
@@ -0,0 +1,13 @@
+public class Quiz {
+    public static void main(String[] args) {
+        System.out.println(appendAll(args));
+    }
+
+    public static String appendAll(String[] args) {
+        String  result = "";
+        for(int i = 0;i < args.length; i++ ){
+            result += args[i] + ";";
+        }
+        return result;
+    }
+}
