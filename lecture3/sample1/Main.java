cb8b41f5c0e56a1a1164586709a8ef8ac0294f16
s1f10210254 <s1f10210248@iniad.org>
2023-10-10 15:16:40 +0900

    lec3

---
+++ lecture3/sample1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Taro";
+        student.grade = 4;
+        System.out.printf("%s %d\n", student.name, student.grade);
+    }
+}
