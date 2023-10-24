42e644d89e9076e47d4b5dc2d93030beaa57e84b
s1f10210254 <s1f10210248@iniad.org>
2023-10-24 15:16:32 +0900

    lec4の模範解答

--- lecture4/sample2/Teacher.java
+++ lecture4/sample2/Teacher.java
@@ -9,8 +9,14 @@ class Teacher extends Member {
         this.title = title;
     }
 
-    public String getTeacherProfile() {
-        return String.format("----- Teacher -----\nID: %s\nName: %s\nTitle: %s\nemail: %s\n",
-                             this.id, this.name, this.title, this.getEmail());
-    }
+    String getTeacherProfile() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("----- Teacher -----\n");
+        sb.append("ID: ").append(id).append("\n");
+        sb.append("Name: ").append(name).append("\n");
+        sb.append("Title: ").append(title).append("\n");
+        String email = getEmail();
+        sb.append("email: ").append(email).append("\n");
+        return sb.toString();
+   }
 }
