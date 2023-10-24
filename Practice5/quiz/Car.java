353db4e4c744ff8678d80fb37654b204e9b81eee
s1f10210254 <s1f10210248@iniad.org>
2023-10-24 16:31:24 +0900

    quiz

---
+++ Practice5/quiz/Car.java
@@ -0,0 +1,21 @@
+public class Car extends Vehicle {
+    private int seat;
+
+    @Override
+    public void start() {
+        System.out.println("Car starts ...");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car stops ...");
+    }
+
+    public Car(int seat) {
+        this.seat = seat;
+    }
+
+    public int getSeat() {
+        return this.seat;
+    }
+}
