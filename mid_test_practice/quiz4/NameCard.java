d990663bd2eaee56d9e146117bfbe8de6468e628
s1f10210254 <s1f10210248@iniad.org>
2023-11-14 11:18:33 +0900

    q4

---
+++ mid_test_practice/quiz4/NameCard.java
@@ -0,0 +1,21 @@
+package quiz4;
+
+public class NameCard {
+    //インスタンスフィールド
+    final String firstName;
+    final String famiyName;
+    final int id;
+    static int cnt;
+
+    //コンストラクタ
+    NameCard(String firstName, String familyName){
+        this.firstName = firstName;
+        this.famiyName = familyName;
+        cnt++;
+        this.id = cnt;
+    }
+
+    String getLabel(){
+        return String.format("No. %d: %s %s", id, firstName, famiyName);
+    }
+}
