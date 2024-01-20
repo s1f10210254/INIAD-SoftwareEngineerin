a2f4908dfec683c022ce4ac3876e10646491e5c6
s1f10210254 <s1f10210248@iniad.org>
2024-01-20 23:42:01 +0900

    Arraysample

--- test/2/ArraySample.java
+++ test/2/ArraySample.java
@@ -1,6 +1,40 @@
 public class ArraySample {
+    //整数の配列の合計を求めるメソッド
+    public static int sum(int[] array){
+        int sum = 0;
+        // for(int i = 0; i<array.length; i++){
+        //     sum += array[i];
+        // }
+        for(int x : array){
+            sum += x;
+        }
+        return sum;
+    }
+    public static int positive(int[] array1){
+        int sum = 0;
+        for(int x : array1){
+            if (x > 0){
+                sum += x;
+            }
+        }
+        return sum;
+    }
+    public static void printOdd(String[] array2) {
+        
+        for(int i = 0; array2.length > i; i++){
+            if(array2[i].length() % 2 == 1){
+                System.out.println(array2[i]);
+            }
+        }
+        
+    }
     public static void main(String[] args){
-        int[] array = new int[3];
-        System.out.println(array[0]);
+        int[] array1 = {1, -2, 3, 4, -5};
+        int result = positive(array1);
+        System.out.println(result);
+
+        String[] array2 = {"Information", "Networking", "for", "Innovation", "And", "Design"};
+        printOdd(array2);
+
     }
 }
