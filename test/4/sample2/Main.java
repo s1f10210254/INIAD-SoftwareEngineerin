1c0cd4766c658d2d817d0658fef3e4c7365dc46b
s1f10210254 <s1f10210248@iniad.org>
2024-01-21 11:09:21 +0900

    member

--- test/4/sample2/Main.java
+++ test/4/sample2/Main.java
@@ -7,5 +7,12 @@ public class Main {
         st.name = "Taro";
         st.grade = 1;
         System.out.printf("%s %s %d\n", st.id, st.name,st.grade);
+        System.out.println(st.getEmail());
+
+        //サブクラスコンストラクタからの継承
+        Student st1 = new Student("2000", "Hiroki", 2);
+        System.out.printf("%s %s %d\n", st1.id, st1.name,st1.grade);
+        System.out.println(st1.getEmail());
+
     }
 }
--- test/4/sample2/Student.java
+++ test/4/sample2/Student.java
@@ -3,4 +3,13 @@ package sample2;
 //Memberクラスを継承
 public class Student extends Member{
     int grade;
+
+    //サブクラスからコンストラクタの継承
+    Student(String id, String name, int grade){
+        super(id, name);
+        this.grade = grade;
+    }
+    Student(){
+        super(null, null);
+    }
 }
