39e9d50fb4ad993cbfe7feb5d03cf8792caaebf8
s1f10210254 <s1f10210248@iniad.org>
2024-01-23 12:23:29 +0900

    2023 final

---
+++ test/example/q2/Main1.java
@@ -0,0 +1,10 @@
+package q2;
+
+public class Main1 {
+    public static void main(String args[]) {
+        Task t1 = new Task("Task A", "Task detail A.");
+        Task t2 = new Task("Task B", "Task detail B.");
+        System.out.println(t1.describe());
+        System.out.println(t2.describe());
+    }
+}
